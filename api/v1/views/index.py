b152101d56500c4c12f0f4382b9da10da6f8ed02
Ben Keener <keener4christ@gmail.com>
2020-04-27 19:07:31 +0000

    Added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,8 @@ Index module
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
@@ -12,3 +14,16 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stats():
+    """
+    Returns JSON of the number of each class
+    """
+    stats = {}
+    stats["amenities"] = storage.count("Amenity")
+    stats["cities"] = storage.count("City")
+    stats["places"] = storage.count("Plae")
+    stats["reviews"] = storage.count("Review")
+    stats["users"] = storage.count("User")
+    return jsonify(stats)

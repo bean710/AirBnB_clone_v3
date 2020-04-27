fca7352ff6d1b6bf98c47d28c86513fd21da2ea4
kevapostol <apostolkevin@gmail.com>
2020-04-27 21:53:19 +0000

    Added empty functions

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,18 +5,42 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 
+
 @app_views.route("/states/<state_id>/cities", methods=["GET"],
                  strict_slashes=False)
+def retrieve_list_city:
+    """
+    Retrieves the list of all City objects of a State
+    """
+
 
 @app_views.route()("/cities/<city_id>", methods=["GET"],
                    strict_slashes=False)
+def retrieve_city_obj:
+    """
+    Retrieves a City object
+    """
+
 
 @app_views.route()("/cities/<city_id>", methods=["DELETE"],
                    strict_slashes=False)
+def del_city:
+    """
+    Deletes a City Object
+    """
+
 
 @app_views.route()("/states/<state_id>/cities", methods=["POST"],
                    strict_slashes=False)
+def create_city:
+    """
+    Creates a City
+    """
+
 
 @app_views.route()("/cities/<city_id>", methods=["PUT"],
-                 strict_slashes=False)
-
+                   strict_slashes=False)
+def update_city:
+    """
+    Updates a City Object
+    """

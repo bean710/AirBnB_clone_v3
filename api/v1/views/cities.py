75319f63d6b13354ed1aee8a4b17635c9141a6ee
kevapostol <apostolkevin@gmail.com>
2020-04-27 21:41:31 +0000

    Added routes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from models import storage
+
+@app_views.route("/states/<state_id>/cities", methods=["GET"],
+                 strict_slashes=False)
+
+@app_views.route()("/cities/<city_id>", methods=["GET"],
+                   strict_slashes=False)
+
+@app_views.route()("/cities/<city_id>", methods=["DELETE"],
+                   strict_slashes=False)
+
+@app_views.route()("/states/<state_id>/cities", methods=["POST"],
+                   strict_slashes=False)
+
+@app_views.route()("/cities/<city_id>", methods=["PUT"],
+                 strict_slashes=False)
+

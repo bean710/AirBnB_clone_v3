87e62c411208eebe97890f454167e7194b4e3329
Ben Keener <keener4christ@gmail.com>
2020-04-27 21:00:34 +0000

    Added GET single state by id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,13 +3,23 @@
 
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from models import storage
+from models.state import State
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
 def all_states():
     """Returns JSON of all of the states"""
-    return jsonify([v.to_dict() for k, v in storage.all().items()])
+    return jsonify([v.to_dict() for k, v in storage.all(State).items()])
 
 
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state(state_id):
+    """Returns a specific state"""
+    state = storage.get("State", state_id)
+
+    if state is None:
+        abort(404)
+    else:
+        return jsonify(state.to_dict())

3e1ef6d29eed22cb554246a48073f134670c4bd2
Ben Keener <keener4christ@gmail.com>
2020-04-29 17:37:08 +0000

    Fixed missing state

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats():
     stats = {}
     stats["amenities"] = storage.count("Amenity")
     stats["cities"] = storage.count("City")
+    stats["states"] = storage.count("State")
     stats["places"] = storage.count("Place")
     stats["reviews"] = storage.count("Review")
     stats["users"] = storage.count("User")
